ee35d3180162dbad7017b870e411993e1afa1114
franciscomarcello17 <franciscomarcello17@gmail.com>
2025-06-09 11:52:32 -0300

    about page

--- public/controllers/SobreController.js
+++ public/controllers/SobreController.js
@@ -14,4 +14,17 @@ angular.module('jogoDaVelhaApp').controller('SobreController',
         vm.isActiveSection = function(section) {
             return vm.activeSection === section;
         };
+
+        // No SobreController
+        vm.openProfile = function(member) {
+            const profiles = {
+                'torrens': 'https://buscatextual.cnpq.br/buscatextual/visualizacv.do?metodo=apresentar&id=K4732940Z2    ',
+                'dumba': 'https://buscatextual.cnpq.br/buscatextual/visualizacv.do?metodo=apresentar&id=K9107865P8',
+                'lima': 'https://br.linkedin.com/in/francisco-marcello-ribeiro-lima-878247265'
+            };
+            
+            if (profiles[member]) {
+                window.open(profiles[member], '_blank');
+            }
+        };
     }]);
